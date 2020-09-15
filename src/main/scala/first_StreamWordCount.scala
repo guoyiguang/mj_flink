7f174f4e3498d46b80e4a31eb401e696340c4b87
guoyiguang <17530152552@163.com>
2020-09-15 16:20:33 +0800

    Initial commit

---
+++ src/main/scala/first_StreamWordCount.scala
@@ -0,0 +1,22 @@
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+object first_StreamWordCount {
+  def main(args: Array[String]): Unit = {
+    // 初始化Flink的Streaming(流计算）上下文执行环境
+    val streamEnv:StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    // 导入隐士转换，建议写在这里，可以防止IDEA代码提示出错的问题
+    import org.apache.flink.streaming.api.scala._
+    //读取数据
+    val stream:DataStream[String] = streamEnv.socketTextStream("127.0.0.1",8888)
+    //转换计算
+    val result:DataStream[(String,Int)] = stream.flatMap(_.split(","))
+      .map((_,1))
+      .keyBy(0)
+      .sum(1)
+
+    //打印到控制台
+    result.print()
+    //启动流式计算，如果没有该代码上面的程序不会执行
+    streamEnv.execute("first_streamwordcount")
+ }
+}
